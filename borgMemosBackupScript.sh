ba16d0a0a01a871c95256458dd2b1a8d58ec5228
Anpleenko Alexander <avanpleenko@gmail.com>
2024-04-16 11:47:30 +0000

    add borgMemosBackupScript

---
+++ borgMemosBackupScript.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+# Setting this, so the repo does not need to be given on the commandline:
+export BORG_REPO='/mnt/nfs/temp/backups/memos'
+
+# See the section "Passphrase notes" for more infos.
+export BORG_PASSPHRASE='memos'
+
+# Log file for script's output named with
+# the script's name, date, and time of execution.
+scriptName=$(basename ${0})
+log="/mnt/nfs/temp/backups/logs/${scriptName}_$(date +%m%d%y%H%M%S).log"
+
+# some helpers and error handling:
+info() { printf "\n%s %s\n\n" "$(date)" "$*" >>$log 2>&1; }
+
+info "Starting backup"
+
+# Create Log
+info "Staring Backup of Memos Database."
+info "------------------------------------------------------------"
+
+# Backup the most important directories into an archive named after
+# the machine this script is currently running on:
+
+/usr/local/bin/borg create \
+    --verbose \
+    --filter AME \
+    --list \
+    --stats \
+    --show-rc \
+    --compression lz4 \
+    --exclude-caches \
+    \
+    ::'{hostname}-{now}' \
+    /home/qa/.memos >>$log 2>&1
+
+backup_exit=$?
+
+info "Pruning repository"
+
+# Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
+# archives of THIS machine. The '{hostname}-*' matching is very important to
+# limit prune's operation to this machine's archives and not apply to
+# other machines' archives also:
+
+/usr/local/bin/borg prune \
+    --list \
+    --glob-archives '{hostname}-*' \
+    --show-rc \
+    --keep-daily 7 \
+    --keep-weekly 4 \
+    --keep-monthly 6 >>$log 2>&1
+
+prune_exit=$?
+
+# actually free repo disk space by compacting segments
+
+info "Compacting repository"
+
+/usr/local/bin/borg compact >>$log 2>&1
+
+compact_exit=$?
+
+# use highest exit code as global exit code
+global_exit=$((backup_exit > prune_exit ? backup_exit : prune_exit))
+global_exit=$((compact_exit > global_exit ? compact_exit : global_exit))
+
+if [ ${global_exit} -eq 0 ]; then
+    info "Backup, Prune, and Compact finished successfully"
+elif [ ${global_exit} -eq 1 ]; then
+    info "Backup, Prune, and/or Compact finished with warnings"
+else
+    info "Backup, Prune, and/or Compact finished with errors"
+fi
+
+# Done
+info "Backup Complete."
+
+exit ${global_exit}
